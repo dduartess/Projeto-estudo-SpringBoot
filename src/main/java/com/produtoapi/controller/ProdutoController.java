7c93dfd3493a9e0275d91a8df95c048a7deae864
dduartess <dandandsoares@gmail.com>
2025-10-29 08:53:09 -0300

    basic CRUD - SpringBoot

---
+++ src/main/java/com/produtoapi/controller/ProdutoController.java
@@ -0,0 +1,50 @@
+package com.produtoapi.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.produtoapi.model.Produto;
+import com.produtoapi.service.ProdutoService;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoService produtoService;
+		
+	@GetMapping
+	public List<Produto> listarTodos(){
+		return produtoService.listarTodos();
+	}
+	
+	@PostMapping
+	public Produto salvar(@RequestBody Produto produto) {
+		return produtoService.salvar(produto);
+	}
+	
+	@PutMapping("/{id}")
+	public Produto atualizar(@PathVariable Long id, @RequestBody Produto produto) {
+		return produtoService.atualizar(id, produto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable Long id) {
+		produtoService.deletar(id);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Produto> findById(@PathVariable Long id){
+		return produtoService.findById(id);
+	}
+}
